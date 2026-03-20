4036375a77e842646c23f508315068db90d16a2b
AldrianLoberiano <loberianorian@gmail.com>
2026-03-20 16:20:58 +0800

    Update urls.py

--- hotel_reservation/urls.py
+++ hotel_reservation/urls.py
@@ -1,13 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.templatetags.static import static as static_file
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("reservations.urls")),
     path("api/", include("reservations.api.urls")),
+    path("favicon.ico", RedirectView.as_view(url=static_file("images/favicon.svg"), permanent=False)),
 ]
 
 if settings.DEBUG:
