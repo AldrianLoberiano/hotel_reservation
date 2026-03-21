ad7862725cda77e7464b6c8d91ab941a4eee6135
AldrianLoberiano <loberianorian@gmail.com>
2026-03-21 11:49:41 +0800

    Update urls.py

--- reservations/urls.py
+++ reservations/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.room_list, name="home"),
     path("register/", views.register_view, name="register"),
     path("rooms/", views.room_list, name="room_list"),
     path("rooms/<int:room_id>/", views.room_detail, name="room_detail"),
